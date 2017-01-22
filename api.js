4ac6bcfbc87bbf80714a729015bcc3b8f638fdd8
jalpesh vadgama <Jalpesh@dotnetjalps.com>
2017-01-22 16:05:41 +0530

    added post and put methods for employee api

--- api.js
+++ api.js
@@ -17,9 +17,69 @@ var employees= [
     }
 ];
 
+// Get all employees
 router.get("/",function (req,res){
     res.json(employees);
 });
+
+/// Add employees
+router.post("/", function (req,res) {
+    var employee = req.body;
+    var isValid =isValidEmployee(employee);
+    if(isValid){
+        employees.push(employee);
+        res.send("/employee/" + employee.Id);
+    } else{
+        res.sendStatus(500);
+    }
+});
+
+//get specific employee based on Id
+router.get("/:Id",function(req,rep){
+    var employeeId = parseInt(req.params.Id);
+    var currentEmployee = employees.filter(e=>e.Id==employeeId)[0];
+    if(!currentEmployee){
+        res.json(currentEmployee);
+    }else{
+        res.sendStatus(204);
+    }
+});
+
+router.put("/:Id",function (req,res) {  
+    var employeeId = parseInt(req.param.Id);
+    var currentEmployee = employees.filter(e=>e.Id==employeeId)[0];
+    if(!currentEmployee){
+        let employee = req.body;
+        var isValid = isValidEmployee(employee);
+        if(!isValid){
+            currentEmployee.FirstName = employee.FirstName;
+            currentEmployee.LastName = employee.FirstName;
+            currentEmployee.Designation = employee.Designation;
+            res.sendStatus(204);
+        }else{
+            res.sendStatus(500);
+        }
+    }
+});
+
+
+
+function isValidEmployee(employee){
+    if(!employee.Id){
+        return false;
+    }
+    if(!employee.FirstName){
+        return false;
+    }
+    if(!employee.LastName){
+        return false;
+    }
+    if(!employee.Designation){
+        return false;
+    }
+    return true;
+}
+
 
 app.use('/api/employee', router);
 app.listen(port);
